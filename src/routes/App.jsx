d625c3f635309cdc3471c61c6a25949e1050933b
KdnaStone <sandra.cadena97@soy.sena.edu.co>
2024-10-29 00:22:50 -0500

    create routes and header files

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+// Importar las librerias necesarias
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// Importar los componentes necesarios
+import Header from '../components/header/Header';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import Home from '../pages/Home';
+import ApiResults from '../pages/ApiResults';
+import Recovery from '../pages/Recovery';
 
 function App() {
   return (
-    <div>App</div>
-  )
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/recovery" element={<Recovery />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/results" element={<ApiResults />} />
+          <Route path="*" element={<div>404 Not Found</div>} />
+        </Routes>
+      </Router>
+
+  );
 }
 
-export default App
+export default App;
