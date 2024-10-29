e820341c00c18addea047ffcdc1e6c32fd7a8905
Sofía Canizales <clsofi64@gmail.com>
2024-10-29 16:38:12 -0500

    se finaliza home y apiResults

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import './home.css'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='bienvenida'>
+      <section>
+        <h3>Bienvenido de vuelta, userName!</h3>
+        <p>Estás conectado a tu cuenta userEmail</p>
+        <p>Explora y disfruta de Disney Personajes</p>
+      </section>
+   </div>
   )
 }
